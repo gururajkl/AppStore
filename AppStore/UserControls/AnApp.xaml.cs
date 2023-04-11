426cd8ccc170902210e05c964d8c32b11a1924be
Gururaj KL <gururajragavendra7@gmail.com>
2023-04-11 09:35:15 +0530

    Initial commit

---
+++ AppStore/UserControls/AnApp.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AppStore.UserControls
+{
+    public partial class AnApp : UserControl
+    {
+        public AnApp()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
